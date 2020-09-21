f19da91f8c7a82ecd3aea0c4f643cad1dfeebe45
KathleenSita <54889622+KathleenSita@users.noreply.github.com>
2020-09-21 21:04:08 +0100

    Jira 1234 testing version

--- src/ak/axone/devintest/git/AppClass2.java
+++ src/ak/axone/devintest/git/AppClass2.java
@@ -4,4 +4,8 @@ public class AppClass2 {
     public void method1(){
         System.out.println("Doing Something");
     }
+
+    public void method1(){
+        System.out.println("Doing Something2 new");
+    }
 }

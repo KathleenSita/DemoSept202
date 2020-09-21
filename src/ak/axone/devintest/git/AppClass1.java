925a1687dc856a84516210a0a522dff286e14652
KathleenSita <54889622+KathleenSita@users.noreply.github.com>
2020-09-21 21:07:43 +0100

    Jira 1234 testing version1

--- src/ak/axone/devintest/git/AppClass1.java
+++ src/ak/axone/devintest/git/AppClass1.java
@@ -4,4 +4,13 @@ public class AppClass1 {
     public void method1(){
         System.out.println("Doing Something");
     }
+    public void method2(){
+        System.out.println("Doing Something");
+    }
+    public void method3(){
+        System.out.println("Doing Something");
+    }
+    public void method4(){
+        System.out.println("Doing Something");
+    }
 }
